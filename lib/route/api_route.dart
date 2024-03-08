08b8c861edb6f2dd619f16a9e7be809188fecf4c
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-03-08 12:08:53 +0330

    Update dependencies

--- lib/route/api_route.dart
+++ lib/route/api_route.dart
@@ -1,10 +1,15 @@
 import 'package:vania/vania.dart';
 
-import 'vesion/v1.dart';
+import 'vesion/version1.dart';
 
 class ApiRoute implements Route {
   @override
   void register() {
     Version1().register();
+
+    Router.get('/', (){
+      return Response.html('<b>Hello World, welcome to Fullstack development with Vania</b>');
+    });
+
   }
 }
